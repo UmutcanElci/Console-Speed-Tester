b871ac624058aec7017c837d6d89fdcca3b927eb
Umutcan Elçi <umutcanelci@yaani.com>
2024-08-30 15:54:04 +0300

    Initial commit

---
+++ SpeedTester/SpeedTest.cs
@@ -0,0 +1,34 @@
+﻿namespace SpeedTester;
+
+using System.Diagnostics;
+
+public class SpeedTest
+{
+    public string Test()
+    {
+        try
+        {
+            using (Process process = new Process())
+            {
+                
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.FileName = "speedtest.exe";
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                
+                //
+                process.Start();
+                string result = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                List<char> results = result.ToList();
+
+                return result.ToString();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
