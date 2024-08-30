7d3e41e26303e18fe09fba94b450b872806c2c16
Umutcan Elci <umutcanelci.me@gmail.com>
2024-08-30 15:54:04 +0300

    Initial commit

---
+++ NetSpeed.App/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using SpeedTester;
+using Spectre.Console;
+string result = new SpeedTest().Test();
+
+ParseAndPrintSpeedTestResult(result);
+
+static void ParseAndPrintSpeedTestResult(string result)
+{
+    
+    string[] delimiters = new string[]
+    {
+        "Speedtest by Ookla",
+        "Server:",
+        "ISP:",
+        "Idle Latency:",
+        "Download:",
+        "Upload:",
+        "Packet Loss:",
+        "Result URL:"
+    };
+
+    foreach (string delimiter in delimiters)
+    {
+        int startIndex = result.IndexOf(delimiter, StringComparison.Ordinal);
+        if (startIndex >= 0)
+        {
+            // Extract section text
+            int endIndex = result.IndexOf('\n', startIndex);
+            if (endIndex == -1)
+                endIndex = result.Length;
+            
+            string line = result.Substring(startIndex, endIndex - startIndex).Trim();
+            
+            AnsiConsole.Markup($"[green]{line}[/]" + "\n");
+        }
+    }
+}
+
